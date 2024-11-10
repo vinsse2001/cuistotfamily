9e08ef5b9805b04e5d3b11e2d49687810764f2b1
Vincent Bossuet <bossuet33@free.fr>
2024-11-10 22:33:50 +0100

    retrait lignes en commentaires (inutiles)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-// import { RouterModule } from '@angular/router';
-// import { AppRoutingModule } from './app-routing.module';
-// import { AccueilComponent } from './accueil/accueil.component';
-// import { RecetteComponent } from './recette/recette.component';
-// import { ProfilComponent } from './profil/profil.component';
 
 @NgModule({
   declarations: [],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
-    // RouterModule,
     AppComponent,
-    // AccueilComponent,
-    // RecetteComponent,
-    // ProfilComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
