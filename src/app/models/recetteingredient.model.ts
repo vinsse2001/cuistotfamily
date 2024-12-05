47de145a9dc6d4a33da76da29476e90f8b6fa569
Vincent Bossuet <bossuet33@free.fr>
2024-12-05 22:22:29 +0100

    mise en forme

--- src/app/models/recetteingredient.model.ts
+++ src/app/models/recetteingredient.model.ts
@@ -1,6 +1,6 @@
 export interface RecetteIngredient {
-    ingredientId: string;   // Référence à l'ID de l'ingrédient dans la liste de référence
-    quantite: number;       // Quantité pour cette recette
-    unite: string;          // Unité de mesure (ex. "g", "ml")
-}
-  
+    ingredientId: string; // ID de l'ingrédient dans la base
+    nom: string;          // Nom de l'ingrédient
+    quantite: number;     // Quantité utilisée dans la recette
+    unite: string;        // Unité de mesure
+ }  
